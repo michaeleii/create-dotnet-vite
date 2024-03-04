fea9eea7705e3da60379a99e5cf2ac56b40a8d45
Michael <michaelleie0ca@gmail.com>
2024-03-04 12:37:07 -0800

    Change paths to be create a new directory in ur current directory

--- lib/utils.ts
+++ lib/utils.ts
@@ -52,54 +52,54 @@ export default defineConfig({
 `;
 
 export async function createNewDotnetWebProject(projectName: string) {
-  await $`cd ../${projectName} && dotnet new sln -n ${projectName}`;
-  await $`cd ../${projectName} && dotnet new web -o ${projectName}.Server`;
-  await $`cd ../${projectName} && dotnet sln add ./${projectName}.Server/${projectName}.Server.csproj`;
-  await $`cd ../${projectName}/${projectName}.Server && dotnet new gitignore`;
+  await $`cd /${projectName} && dotnet new sln -n ${projectName}`;
+  await $`cd /${projectName} && dotnet new web -o ${projectName}.Server`;
+  await $`cd /${projectName} && dotnet sln add ./${projectName}.Server/${projectName}.Server.csproj`;
+  await $`cd /${projectName}/${projectName}.Server && dotnet new gitignore`;
 
   // Add DotNetEnv package
-  await $`cd ../${projectName}/${projectName}.Server && dotnet add package DotNetEnv`;
+  await $`cd /${projectName}/${projectName}.Server && dotnet add package DotNetEnv`;
   // Copy the .env.example file to the server project
-  write(`../${projectName}/${projectName}.Server/.env`, envExample);
+  write(`/${projectName}/${projectName}.Server/.env`, envExample);
 
   // Add Swagger
-  await $`cd ../${projectName}/${projectName}.Server && dotnet add package Swashbuckle.AspNetCore`;
-  await $`cd ../${projectName}/${projectName}.Server && dotnet add package Microsoft.AspNetCore.OpenApi`;
+  await $`cd /${projectName}/${projectName}.Server && dotnet add package Swashbuckle.AspNetCore`;
+  await $`cd /${projectName}/${projectName}.Server && dotnet add package Microsoft.AspNetCore.OpenApi`;
 
   // Add Entity Framework
-  await $`cd ../${projectName}/${projectName}.Server && dotnet add package Microsoft.EntityFrameworkCore`;
-  await $`cd ../${projectName}/${projectName}.Server && dotnet add package Microsoft.EntityFrameworkCore.Design`;
+  await $`cd /${projectName}/${projectName}.Server && dotnet add package Microsoft.EntityFrameworkCore`;
+  await $`cd /${projectName}/${projectName}.Server && dotnet add package Microsoft.EntityFrameworkCore.Design`;
 
   // Copy templates/dotnet/Program.txt to the project directory
 
-  await write(`../${projectName}/${projectName}.Server/Program.cs`, program);
+  await write(`/${projectName}/${projectName}.Server/Program.cs`, program);
 }
 
 export async function createNewViteProject(projectName: string) {
   // Create the vite project
-  await $`cd ../${projectName} && bun create vite ${projectName.toLowerCase()}-client --template react-ts`;
+  await $`cd /${projectName} && bun create vite ${projectName.toLowerCase()}-client --template react-ts`;
 
   // Rename the project directory
-  await $`mv ../${projectName}/${projectName.toLowerCase()}-client ../${projectName}/${projectName}.Client`;
+  await $`mv /${projectName}/${projectName.toLowerCase()}-client /${projectName}/${projectName}.Client`;
 
   // Install the dependencies
-  await $`cd ../${projectName}/${projectName}.Client && bun install`;
+  await $`cd /${projectName}/${projectName}.Client && bun install`;
 
   // Remove the default App.css
-  await rm(`../${projectName}/${projectName}.Client/src/App.css`, {
+  await rm(`/${projectName}/${projectName}.Client/src/App.css`, {
     force: true,
   });
   // Remove the default App.tsx
-  await rm(`../${projectName}/${projectName}.Client/src/App.tsx`, {
+  await rm(`/${projectName}/${projectName}.Client/src/App.tsx`, {
     force: true,
   });
 
   // Copy templates/vite/App.txt to the project directory
-  await write(`../${projectName}/${projectName}.Client/src/App.tsx`, app);
+  await write(`/${projectName}/${projectName}.Client/src/App.tsx`, app);
 
   // Read the launchSettings.json file to get the dotnet api url
   const launchSettings = await file(
-    `../${projectName}/${projectName}.Server/Properties/launchSettings.json`
+    `/${projectName}/${projectName}.Server/Properties/launchSettings.json`
   ).json();
 
   // Copy templates/vite/vite.config.txt to the project directory
@@ -114,20 +114,20 @@ export async function createNewViteProject(projectName: string) {
   // Replace the projectName in the vite.config.ts file
 
   await write(
-    `../${projectName}/${projectName}.Client/vite.config.ts`,
+    `/${projectName}/${projectName}.Client/vite.config.ts`,
     viteConfig
   );
 }
 
 export async function setupTailwind(projectName: string) {
-  await $`cd ../${projectName}/${projectName}.Client && bun install -D tailwindcss postcss autoprefixer prettier prettier-plugin-tailwindcss`;
-  await $`cd ../${projectName}/${projectName}.Client && bun tailwindcss init -p`;
+  await $`cd /${projectName}/${projectName}.Client && bun install -D tailwindcss postcss autoprefixer prettier prettier-plugin-tailwindcss`;
+  await $`cd /${projectName}/${projectName}.Client && bun tailwindcss init -p`;
 
   // Remove the default tailwind.config.js
-  await $`rm -f ../${projectName}/${projectName}.Client/tailwind.config.js`;
+  await $`rm -f /${projectName}/${projectName}.Client/tailwind.config.js`;
 
   // Remove the default index.css
-  await $`rm -f ../${projectName}/${projectName}.Client/src/index.css`;
+  await $`rm -f /${projectName}/${projectName}.Client/src/index.css`;
 
   // Copy templates/tailwind.config.js to the project directory
   const tailwindConfig = `/** @type {import('tailwindcss').Config} */
@@ -140,7 +140,7 @@ export default {
 };
   `;
   await write(
-    `../${projectName}/${projectName}.Client/tailwind.config.js`,
+    `/${projectName}/${projectName}.Client/tailwind.config.js`,
     tailwindConfig
   );
   // Copy templates/index.css to the project directory
@@ -149,10 +149,7 @@ export default {
 @tailwind utilities;
 
   `;
-  await write(
-    `../${projectName}/${projectName}.Client/src/index.css`,
-    indexCss
-  );
+  await write(`/${projectName}/${projectName}.Client/src/index.css`, indexCss);
 
   // Copy templates/tailwind/prettier.config.js to the project directory
   const prettierConfig = `/** @type {import("prettier").Config} */
@@ -161,7 +158,7 @@ export default {
 };
   `;
   await write(
-    `../${projectName}/${projectName}.Client/prettier.config.js`,
+    `/${projectName}/${projectName}.Client/prettier.config.js`,
     prettierConfig
   );
 }
